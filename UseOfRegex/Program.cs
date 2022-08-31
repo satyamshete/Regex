cb0b6dc8838140005ea146e5be4343fd2a9d2c7d
satyamshete <shetesatyam99@gmail.com>
2022-08-31 22:56:38 +0530

    [Satyam] UC1 First Name with First letter capital

---
+++ UseOfRegex/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UseOfRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Regex Problems");
+            RegexProblem regexProblem = new RegexProblem();
+
+            ////UC1 
+            regexProblem.CheckFirstName("Viraj");
+        }
+    }
+}
---
+++ UseOfRegex/RegexProblem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UseOfRegex
+{
+    internal class RegexProblem
+    {
+        public Regex FirstName = new Regex(@"[A-Z]{1}[A-z a-z]{2,}");   ///UC1
+
+
+        public void CheckFirstName(string fName)   ///UC1
+        {
+            Console.WriteLine("first name is : " + fName);
+            if (FirstName.IsMatch(fName))
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name");
+            }
+            Console.WriteLine();
+        }
+    }
+}
